e19c8e0ebf0207225600a5a515a7688524c43d26
marcos <marcospiccotto12@gmail.com>
2023-06-13 18:54:58 -0300

    movimiento basico personaje

--- app.s
+++ app.s
@@ -6,16 +6,38 @@
 	.equ GPIO_GPFSEL0, 0x00
 	.equ GPIO_GPLEV0,  0x34
 
-	.equ KEY_W, 0b00000010  // Valor del GPIO para la tecla W
-    .equ KEY_L, 0b00000001  // Valor del GPIO para la tecla L
+    .equ KEY_W, 0b00000010
+    .equ KEY_A, 0b00000100
+    .equ KEY_S, 0b00001000
+    .equ KEY_D, 0b00010000
+    .equ KEY_SPACE, 0b00100000
 
 	.globl main
 
-FunctionCuadrado:
-//CORDENADAS
+	FunctionCuadrado:
+
+    add x14, x2, x15 // x14 = PosY + 
+	mov x21 , SCREEN_HEIGH
+    cmp x21, x14 
+    b.lt parseX2_1
+    cmp x2, 0
+	b.lt parseX2_2
+	b continue1
+
+parseX2_1:
+	mov x2, SCREEN_HEIGH
+	sub x2, x2, x16
+	b continue1
+
+parseX2_2:
+	mov x2, 0
+	b continue1
+
+continue1:
 	mov x14, x2
 loopY:
 	mov x13, x1
+
 loopX:
 	mul x11, x14, x12
 	add x11, x11, x13 // x11 = x +( y * 640)
@@ -24,7 +46,7 @@ loopX:
 	
 	stur w10,[x11]
 	add x13, x13, 1
-	add x11, x1, x9 //ANCHO
+	add x11, x1, x16 //ANCHO
 	sub x11, x13, x11 // x13 = 0, 1, 2, ... , 199 
 	cmp x11, 0 
 	bge exitX
@@ -39,186 +61,102 @@ exitX:
 exit:
 	ret
 
-FunctionWindows :
-	//WINDOWS
-	bl FunctionFondo
-	movz x10, 0xFF, lsl 16
-    movk x10, 0x2E01, lsl 00
-
-	mov x9, 120 //Ancho
-	mov x15, 80 //Alto
-	mov x2, 120
-	mov x1, 160 
-	bl FunctionCuadrado
-	//-----------------------------
-	movz x10, 0x00, lsl 16
-    movk x10, 0x0D7B, lsl 00
-
-	mov x2, 205
-	mov x1, 160 
-	bl FunctionCuadrado
-	//-----------------------------
-	movz x10, 0x0B, lsl 16
-    movk x10, 0x8A48, lsl 00
-
-	mov x2, 120
-	mov x1, 285 
-	bl FunctionCuadrado
-	//-----------------------------
-	movz x10, 0xFF, lsl 16
-    movk x10, 0xE504, lsl 00
-
-	mov x2, 205
-	mov x1, 285
-	bl FunctionCuadrado
-	//-----------------------------
-	
-	ret
-
-FunctionLinux:
-
-	//LINUX
-	bl FunctionFondo
-	movz x10, 0x00, lsl 16
-    movk x10, 0x0001, lsl 00
-
-	mov x9, 210 //Ancho
-	mov x15, 300 //Alto
-	mov x2, 210
-	mov x1, 200 
-	bl FunctionCuadrado
-
-	movz x10, 0xFF, lsl 16
-    movk x10, 0xFFFF, lsl 00
-
-	mov x9, 150 //Ancho
-	mov x15, 300 //Alto
-	mov x2, 230
-	mov x1, 230 
-	bl FunctionCuadrado
-
-	movz x10, 0x00, lsl 16
-    movk x10, 0x0001, lsl 00
-
-	mov x9, 125 //Ancho
-	mov x15, 100 //Alto
-	mov x2, 110
-	mov x1, 240 
-	bl FunctionCuadrado
-
-	movz x10, 0xFF, lsl 16
-    movk x10, 0xFFFF, lsl 00
-
-	mov x9, 95 //Ancho
-	mov x15, 50 //Alto
-	mov x2, 160
-	mov x1, 255
-	bl FunctionCuadrado
-
-	movz x10, 0xFF, lsl 16
-    movk x10, 0xFFFF, lsl 00
-
-	mov x9, 40 //Ancho
-	mov x15, 50 //Alto
-	mov x2, 120
-	mov x1, 255
-	bl FunctionCuadrado
-	
-	movz x10, 0xFF, lsl 16
-    movk x10, 0xFFFF, lsl 00
-
-	mov x9, 40 //Ancho
-	mov x15, 50 //Alto
-	mov x2, 120
-	mov x1, 310
-	bl FunctionCuadrado
-
-	movz x10, 0xFC, lsl 16
-    movk x10, 0x4B08, lsl 00
-
-	mov x9, 45 //Ancho
-	mov x15, 30 //Alto
-	mov x2, 160
-	mov x1, 280
-	bl FunctionCuadrado
-
-	mov x9, 25 //Ancho
-	mov x15, 50 //Alto
-	mov x2, 160
-	mov x1, 290
-	bl FunctionCuadrado
-
-	movz x10, 0x00, lsl 16
-    movk x10, 0x0000, lsl 00
-
-	mov x9, 15 //Ancho
-	mov x15, 25 //Alto
-	mov x2, 130
-	mov x1, 320
-	bl FunctionCuadrado
-
-	mov x9, 15 //Ancho
-	mov x15, 25 //Alto
-	mov x2, 130
-	mov x1, 270
-	bl FunctionCuadrado
-
-	ret
-	
 FunctionFondo:
-		movz x10, 0x00, lsl 16
-		movk x10, 0x1AFD, lsl 00
-		mov x2, SCREEN_HEIGH         // Y Size
-	loop1:
-		mov x1, SCREEN_WIDTH         // X Size
-	loop0:
-		stur w10,[x0]  // Colorear el pixel N
-		add x0,x0,4    // Siguiente pixel
-		sub x1,x1,1    // Decrementar contador X
-		cbnz x1,loop0  // Si no terminó la fila, salto
-		sub x2,x2,1    // Decrementar contador Y
-		cbnz x2,loop1  // Si no es la última fila, salto
+        mov x18, SCREEN_HEIGH         // Y Size
+    loop1:
+        mov x19, SCREEN_WIDTH         // X Size
+    loop0:
+        stur w10,[x0]  // Colorear el pixel N
+        add x0,x0,4    // Siguiente pixel
+        sub x19,x19,1    // Decrementar contador X
+        cbnz x19,loop0  // Si no terminó la fila, salto
+        sub x18,x18,1    // Decrementar contador Y
+        cbnz x18,loop1  // Si no es la última fila, salto
 
-		mov x0, x20 // Guarda la dirección base del framebuffer en x0
-		mov x12, 640 //Salto de cada cordenada Y
-	ret
+        mov x0, x20 // Guarda la dirección base del framebuffer en x0
+        mov x12, 640 //Salto de cada cordenada Y
+    ret
 
 main:
-    // x0 contiene la direccion base del framebuffer
-    mov x20, x0 // Guarda la dirección base del framebuffer en x20
-    //---------------- CODE HERE ------------------------------------
+	// x0 contiene la direccion base del framebuffer
+	mov x20, x0 // Guarda la dirección base del framebuffer en x20
+	//---------------- CODE HERE ------------------------------------
+
+	// Ejemplo de uso de gpios
+    
+    // Atención: se utilizan registros w porque la documentación de broadcom
+    // indica que los registros que estamos leyendo y escribiendo son de 32 bits
+
+    // Setea gpios 0 - 9 como lectura
+    
+
+    //---------------------------------------------------------------
+    // Infinite Loop
+    mov x9, GPIO_BASE
+    str wzr, [x9, GPIO_GPFSEL0]
+
+    movz x10, 0x00, lsl 16
+    movk x10, 0x1AFD, lsl 00
 	bl FunctionFondo
 
-    // Ejemplo de uso de gpios
-    mov x9, GPIO_BASE
-
-    // Configura gpios 0 - 9 como entradas
-    ldr x10, [x9, GPIO_GPFSEL0]   // Lee el valor actual de GPIO_GPFSEL0
-    bfi x10, xzr, #0, #3          // Establece los bits 0-2 como ceros (modo de entrada)
-    str x10, [x9, GPIO_GPFSEL0]   // Guarda el valor modificado en GPIO_GPFSEL0
+    mov x2, 0
+	mov x1, 200
 
 InfLoop:
-    // Lee el estado de los GPIO 0 - 31
-    ldr w10, [x9, GPIO_GPLEV0]
+    movz x10, 0xFF, lsl 16
+    movk x10, 0xFFFF, lsl 00
+    mov x16, 50 //Ancho
+    mov x15, 50 //Alto
+    bl FunctionCuadrado
 
-    cmp w10, KEY_W
-    beq WindowsCheck
+    ldr w10, [x9, GPIO_GPLEV0]
+    and w11, w10, KEY_S
+    cmp w11, KEY_S
+    beq MoveDown
+
+    ldr w10, [x9, GPIO_GPLEV0]
+    and w11, w10, KEY_D
+    cmp w11, KEY_D
+    beq MoveRigth
 
 	ldr w10, [x9, GPIO_GPLEV0]
-	cmp w10, KEY_L
-    beq LinuxCheck
+    and w11, w10, KEY_W
+    cmp w11, KEY_W
+    beq MoveUp
 
-	bl InfLoop
-LinuxCheck:
-    // Lee el estado de los GPIO 0 - 31 nuevamente para comprobar la tecla L
-    ldr w10, [x9, GPIO_GPLEV0]
+	ldr w10, [x9, GPIO_GPLEV0]
+    and w11, w10, KEY_A
+    cmp w11, KEY_A
+    beq MoveLeft
 
-    cmp w10, KEY_L
-      // Si no se presionó la tecla L, vuelve al bucle principal
-
-    bl FunctionLinux
     b InfLoop
 
-WindowsCheck:
-    bl FunctionWindows
+MoveDown:
+    add x2, x2, 1
+	movz x10, 0x00, lsl 16
+    movk x10, 0x1AFD, lsl 00
+    bl FunctionFondo
     b InfLoop
+
+MoveRigth:
+    add x1, x1, 1
+	movz x10, 0x00, lsl 16
+    movk x10, 0x1AFD, lsl 00
+    bl FunctionFondo
+    b InfLoop
+
+MoveUp:
+    sub x2, x2, 1
+	movz x10, 0x00, lsl 16
+    movk x10, 0x1AFD, lsl 00
+	bl FunctionFondo
+    b InfLoop
+
+MoveLeft:
+    sub x1, x1, 1
+	movz x10, 0x00, lsl 16
+    movk x10, 0x1AFD, lsl 00
+	bl FunctionFondo
+    b InfLoop
+
+
